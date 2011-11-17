0d9eeb343117c7e90ffd3c3211d7521f9c069f3b
adrien.melotte@free.fr <adrien.melotte@free.fr@b3a628c3-f253-58a1-da58-471f93719afe>
2011-11-17 22:18:48 +0000

    Semaphore: modification des classes FeuBicolore et FeuTricolore

--- trunk/COO_tr/src/signalisation/FeuBicolore.java
+++ trunk/COO_tr/src/signalisation/FeuBicolore.java
@@ -1,5 +1,14 @@
 package signalisation;
 
+import train.Train;
+
 public class FeuBicolore extends Semaphore{
+
+	int vitesse=0;
+	
+	void modifTrain(Train t) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
